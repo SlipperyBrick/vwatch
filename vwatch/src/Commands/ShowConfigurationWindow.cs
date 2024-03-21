2ec4159c684f3ad97b22ad721411db0a925f696a
SlipperyBrick <dev.davejames@gmail.com>
2024-03-21 09:45:44 +0000

    Set up frontend window for configuration of vwatch. Added resources for fonts and icons

---
+++ vwatch/src/Commands/ShowConfigurationWindow.cs
@@ -0,0 +1,14 @@
+﻿using vwatch.Controls;
+
+namespace vwatch
+{
+    [Command(PackageIds.ShowConfigurationWindow)]
+    internal sealed class ShowConfigurationWindow : BaseCommand<ShowConfigurationWindow>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            ConfigurationWindow window = new ConfigurationWindow();
+            window.Show();
+        }
+    }
+}
---
+++ vwatch/src/Controls/ConfigurationWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace vwatch.Controls
+{
+    public partial class ConfigurationWindow : Window
+    {
+        public ConfigurationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
