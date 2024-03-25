a6ed6a523591a324f73d581e11c5d28cab29eec3
SlipperyBrick <dev.davejames@gmail.com>
2024-03-25 20:58:33 +0000

    Implemented closing window logic for both Ok and Close buttons

---
+++ vwatch/src/Behaviors/ConfigurationWindowClose.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace vwatch.Behaviors
+{
+    public class ConfigurationWindowClose : Behavior<Button>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.Click += OnButtonClick;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.Click -= OnButtonClick;
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this.AssociatedObject);
+            window?.Close();
+        }
+    }
+}
